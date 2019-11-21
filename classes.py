d7f4ad9b79d8db1be0c8bacc56b55073311b4c24
Rachel Ng <rachel.ng.135@gmail.com>
2019-11-21 15:05:10 -0500

    did an oopsies

--- classes.py
+++ classes.py
@@ -79,9 +79,9 @@ def seek(fulfills, reqs, wi, course_desc, n, hardness, out, fulfilled="-", comp=
 
     if comp == "==":
         if w == "WI":
-            ok = {i : reqs[i] for i in fulfills if fulfills[i] >= int(n) and "WI" in reqs[i]}
+            ok = {i : reqs[i] for i in fulfills if fulfills[i] == int(n) and "WI" in reqs[i]}
         elif w == "-WI":
-            ok = {i : reqs[i] for i in fulfills if fulfills[i] >= int(n) and "WI" not in reqs[i]}
+            ok = {i : reqs[i] for i in fulfills if fulfills[i] == int(n) and "WI" not in reqs[i]}
         else:
             ok = {i : reqs[i] for i in fulfills if fulfills[i] == int(n)}
 
